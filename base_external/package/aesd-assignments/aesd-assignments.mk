94bf727e6708429ed699f47ad0e0d1e249c462d6
sid <sidorov@mail.ioffe.ru>
2023-05-12 23:24:44 +0200

    Assignment 8. initial. full-test passed. Changed commit for code repo (aesdsocket open/close file every packet). runquemu.sh added port 9000

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = cd3da48cdfc0832111ad2ff80495499db73bbef8
+AESD_ASSIGNMENTS_VERSION = a3e402fcb8a1bea037f8aa5528cc0d043ee526ce
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesd-char/aesd-char.mk
+++ base_external/package/aesd-char/aesd-char.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_CHAR_VERSION = 800b4d7cd71431ad47f000db946c16d8b4f756df
+AESD_CHAR_VERSION = a3e402fcb8a1bea037f8aa5528cc0d043ee526ce
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
